7fe302b35a64f8d39da3317b145236885c8b6148
Mahdieh <mahdeh.abed@gmail.com>
2021-11-02 23:46:00 +0330

    tamrin-1-done

---
+++ tamrin-1/tamrin-1.js
@@ -0,0 +1,39 @@
+// 1 )تابعی بنویسید به نام contain که مقداری را در یک object تودرتو جستجو می کند. اگر object حاوی آن مقدار باشد، مقدار
+// true را برمی گرداند. مثال:
+// 20 , Object را دریافت کرده true را برمیگرداند
+
+const obj = {
+    d: {
+        c: {
+            b: {
+                a: {
+                    number: 20,
+                    str: 'hi',
+                }
+            }
+        }
+    }
+}
+
+function contain(object, input) {
+    let arr=Object.values(object)
+    for (let i = 0; i <= Object.values(object).length; i++) {
+        if (arr[i] === input) {
+            return true
+        }
+        if(typeof arr[i]==='object'){
+           return contain(arr[i],input)
+        }
+    }
+    return false
+}
+console.log(contain(obj, 20))
+console.log(contain(obj, 'hi'))
+
+
+
+
+
+
+
+
